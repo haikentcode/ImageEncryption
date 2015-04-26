8e48958d64e6f85e43c7d3a4e05c52672c19377f
haikentcode <hiteshnitj16@gmail.com>
2015-04-27 03:16:17 +0530

    update

---
+++ decodeImg.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import sys
+x=sys.argv[1]
+y=sys.argv[2]
+z=sys.argv[3]
+
+hk=open(x,"rb")
+tm=hk.read()
+ag=open(z,'wb')
+gudan=sum(ord(ch) for ch in y)%256
+list1=list(tm)
+k=0;
+ 
+for x in list1:
+        i=int(x.encode('hex'),16)
+        t=(i-gudan)%256
+        list1[k]=("%0.2X"%t).decode('hex')
+        k=k+1;
+
+strn=''.join(list1)
+
+ag.write(strn);
+
