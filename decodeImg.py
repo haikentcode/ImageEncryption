6357b632bce96df9464e6eff38c3ff425f782663
haikentcode <hiteshnitj16@gmail.com>
2015-08-06 01:20:36 +0530

    ck

--- decodeImg.py
+++ decodeImg.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
 import sys
-x=sys.argv[1]
-y=sys.argv[2]
-z=sys.argv[3]
+imageName=sys.argv[1]
+password=sys.argv[2]
+outPutImageName=sys.argv[3]
 
-hk=open(x,"rb")
+hk=open(imageName,"rb")
 tm=hk.read()
-ag=open(z,'wb')
-gudan=sum(ord(ch) for ch in y)%256
+ag=open(outPutImageName,'wb')
+listp=list(password);
 list1=list(tm)
 k=0;
- 
+kt=0; 
 for x in list1:
         i=int(x.encode('hex'),16)
+        gudan=ord(listp[kt])%256
+        kt=kt+1
+        if kt>=len(listp):
+        	kt=0
         t=(i-gudan)%256
         list1[k]=("%0.2X"%t).decode('hex')
         k=k+1;
